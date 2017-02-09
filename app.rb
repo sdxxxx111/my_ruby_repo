cb0cbc4e58fbdd551d18c5f953045ad7e78995ee
kato <mail1233@example.com>
2017-02-09 21:39:06 +0900

    updatefile

--- app.rb
+++ app.rb
@@ -2,22 +2,20 @@ require 'sequel'
 require 'sinatra'
 require 'sinatra/reloader'
 
-db_path = File.dirname(__FILE__) + "dbtable.db"
+db_path = File.dirname(__FILE__) + "/dbtable.db"
 DB = Sequel.sqlite(db_path)
 
 get '/' do
  erb :index
 end
 
-
-
-# post '/comment' do
-#   if params[:name]
-#     u_name = params[:name]
-#   else
-#     u_name = "ななし"
-#   end
-#   message = params[:umessage]
-#
-#   DB[:messagetable].insert(name: u_name, message: message)
-# end
+post '/comment' do
+  if params[:uname]
+    u_name = params[:uname]
+  else
+    u_name = "ななし"
+  end
+  message = params[:umessage]
+ puts "#{u_name}#{message}"
+  DB[:messagetable].insert(name: u_name, message: message)
+end
