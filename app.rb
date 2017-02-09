5470023c5ef58f69674adac246f0747a00af01a1
kato <mail1233@example.com>
2017-02-09 20:28:58 +0900

    addfile

---
+++ app.rb
@@ -0,0 +1,21 @@
+require 'sequel'
+require 'sinatra'
+
+db_path = File.dirname(__FILE__) + "/dbtable.db"
+DB = Sequel.sqlite(db_path)
+
+
+get '/' do
+  'hello 2017'
+end
+
+# post '/comment' do
+#   if params[:name]
+#     u_name = params[:name]
+#   else
+#     u_name = "ななし"
+#   end
+#   message = params[:umessage]
+#
+#   DB[:messagetable].insert(name: u_name, message: message)
+# end
